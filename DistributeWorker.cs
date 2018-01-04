e567c806a61d9edaf8890a54b8a1e868b9206e53
Anders Selborn <nroblex@hotmail.com>
2018-01-04 22:46:09 +0100

    minor.

--- DistributeWorker.cs
+++ DistributeWorker.cs
@@ -1,24 +1,59 @@
-﻿using System.IO.Compression;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
 using System.Threading.Tasks;
 namespace DistributeLib
 {
-    public class DistributeWorker
+    public class DistributeWorker : QueueManager
     {
         GitUtilCommnad mGitUtilCommand;
         public DistributeWorker(GitUtilCommnad command)
         {
-            mGitUtilCommand = command;
-            var t = ExecuteCommand();
+            
+            var task = ExecuteCommand();
+
+            String s = "anders";
         }
 
-        private async Task ExecuteCommand()
+     
+        private async Task<Boolean> ExecuteCommand()
         {
+            bool result = false;
             await Task.Run(() =>
             {
                 //mGitUtilCommand.PathToBuild
-                ZipFile.CreateFromDirectory(mGitUtilCommand.PathToBuild, mGitUtilCommand.PathToBuild + "\\" + mGitUtilCommand.BuildNumber + ".zip");
-                
+                try
+                {
+                    ZipFile.CreateFromDirectory(@mGitUtilCommand.PathToBuild,
+                        new DirectoryInfo(mGitUtilCommand.PathToBuild).Parent.FullName + "\\" + mGitUtilCommand.BuildNumber + ".zip", CompressionLevel.Optimal, true);
+                    result = true;
+                }
+                catch (Exception ep)
+                {
+                    result = false;
+                }
+
+
             });
+
+            return result;
         }
+
+
+        private async Task<bool> ExecuteAndZip()
+        {
+            await Task.Run(() =>
+           {
+               ZipFile.CreateFromDirectory(mGitUtilCommand.PathToBuild,
+                    new DirectoryInfo(mGitUtilCommand.PathToBuild).Parent + "\\" + mGitUtilCommand.BuildNumber + ".zip", CompressionLevel.Optimal, true);
+
+
+           });
+
+            return true;
+
+        }
+
+
     }
 }
