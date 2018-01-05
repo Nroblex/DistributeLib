bd106716b4ff9a54dc957e2875538839d2299d7b
Anders Selborn <anders.selborn@wsp.com>
2018-01-05 12:16:34 +0100

    tasks

--- DistributeWorker.cs
+++ DistributeWorker.cs
@@ -10,22 +10,23 @@ namespace DistributeLib
         public delegate long dlgOnExecuteFinished();
         public event dlgOnExecuteFinished eventOnZipfileFinished;
 
-        
+
         public DistributeWorker(GitUtilCommnad command)
         {
             mGitUtilCommand = command;
             //var task = ExecuteCommand();
 
-            Task<long> T = new Task<long>(CreateZipArchive);
-            T.ContinueWith(OnZipFileFinished);
-            
-            T.Start();
+            //Task<long> T = new Task<long>(CreateZipArchive);
+            //T.ContinueWith(OnZipFileFinished);
+            //T.Start();
+
+            var result = CreateZipArchive();
             
         }
 
         private void OnZipFileFinished(Task obj)
-        {            
-            
+        {
+
             eventOnZipfileFinished += DistributeWorker_Finished;
             eventOnZipfileFinished.Invoke();
         }
@@ -65,18 +66,62 @@ namespace DistributeLib
             }
             catch { }
         }
-        private long CreateZipArchive()
+        private async Task CreateZipArchive()
+        {
+            Func<long> function = new Func<long>(() => ZipIt());
+            long result = await Task.Run(function);
+
+            /*
+            DirectoryInfo dir = new DirectoryInfo(mGitUtilCommand.PathToBuild);
+            FileInfo resultZip = null;
+
+            if (dir.Parent.Parent == null)
+            {
+
+
+                await Task.Run(() =>
+                {
+                    DropExistingArchive(new FileInfo(new DirectoryInfo(mGitUtilCommand.PathToBuild).Root + "\\" + mGitUtilCommand.BuildNumber + ".zip"));
+
+                    ZipFile.CreateFromDirectory(mGitUtilCommand.PathToBuild,
+                    new DirectoryInfo(mGitUtilCommand.PathToBuild).Root + "\\" + mGitUtilCommand.BuildNumber + ".zip", CompressionLevel.Optimal, true);
+                    resultZip = new FileInfo(new DirectoryInfo(mGitUtilCommand.PathToBuild).Root + "\\" + mGitUtilCommand.BuildNumber + ".zip");
+
+                });
+
+            }
+            else
+            {
+                await Task.Run(() =>
+                {
+                    DropExistingArchive(new FileInfo(new DirectoryInfo(mGitUtilCommand.PathToBuild).Parent.FullName + "\\" + mGitUtilCommand.BuildNumber + ".zip"));
+
+                    ZipFile.CreateFromDirectory(mGitUtilCommand.PathToBuild,
+                        new DirectoryInfo(mGitUtilCommand.PathToBuild).Parent.FullName + "\\" + mGitUtilCommand.BuildNumber + ".zip", CompressionLevel.Optimal, true);
+                    resultZip = new FileInfo(new DirectoryInfo(mGitUtilCommand.PathToBuild).Parent.FullName + "\\" + mGitUtilCommand.BuildNumber + ".zip");
+                });
+
+            }
+            return resultZip.Length;
+
+            */
+        }
+
+        private long ZipIt()
         {
             DirectoryInfo dir = new DirectoryInfo(mGitUtilCommand.PathToBuild);
             FileInfo resultZip = null;
 
             if (dir.Parent.Parent == null)
             {
+
                 DropExistingArchive(new FileInfo(new DirectoryInfo(mGitUtilCommand.PathToBuild).Root + "\\" + mGitUtilCommand.BuildNumber + ".zip"));
 
                 ZipFile.CreateFromDirectory(mGitUtilCommand.PathToBuild,
-                    new DirectoryInfo(mGitUtilCommand.PathToBuild).Root + "\\" + mGitUtilCommand.BuildNumber + ".zip", CompressionLevel.Optimal, true);
+                new DirectoryInfo(mGitUtilCommand.PathToBuild).Root + "\\" + mGitUtilCommand.BuildNumber + ".zip", CompressionLevel.Optimal, true);
                 resultZip = new FileInfo(new DirectoryInfo(mGitUtilCommand.PathToBuild).Root + "\\" + mGitUtilCommand.BuildNumber + ".zip");
+
+
             }
             else
             {
